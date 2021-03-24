122f852a05f68508f4701eb009b5696adc913148
ADAM ANDREASON <adamandreason@gmail.com>
2021-03-24 13:16:32 -0600

    removed duplicate about button

--- src/Landing.svelte
+++ src/Landing.svelte
@@ -492,67 +492,7 @@
       <Button color="secondary" on:click={toggleModal}>CLOSE</Button>
     </ModalFooter>
   </Modal>
-  <div class="float">
-    <Button color="secondary" on:click={toggle2}>About</Button>
-    <Modal isOpen={isOpen2} toggle={toggle2} size="lg">
-      <ModalHeader toggle={toggle2}>About 1800's Clothing</ModalHeader>
-      <ModalBody>
-        <div class="overflow-auto aboutDiv">
-          <div>
-            <img
-              src="assets/carma.jpg"
-              alt="Carma de Jong Anderson"
-              loading="lazy"
-            />
-          </div>
-
-          <div>
-            <p>
-              All her life, Carma de Jong Anderson was fascinated with
-              historical costumes. As she grew to maturity, her interest focused
-              on clothing of the 1800s. On numerous occasions she was asked by
-              the Church of Jesus Christ of Latter Day Saints to consult with
-              them on the restoration of historic sites of the Church, and the
-              dressing of the docents there who represented families of those
-              decades. Unfortunately, modern artists are not very familiar with
-              authentic clothing worn by the people of the 1800s. Their
-              paintings and illustrations often depict wild guesses as to what
-              was worn.
-            </p>
-            <p>
-              So Carma took it upon herself for over 40 years to travel
-              extensively to museums, art galleries, historic sites, and to pour
-              through images in antique books and magazines collecting detailed
-              images of what people in the 1800s really wore. She earned a Ph.D.
-              at BYU with a large, 3 volume doctoral dissertation showing what
-              she had learned. Limited to a printing of only 6 three-volume
-              copies, It was much in demand with artists in and outside of the
-              Church of Jesus Christ LDS. She was asked to update it, enlarge
-              it, and eventually put it online so anyone in the world could see
-              the results of her decades of research, showing over 2000 images
-              of correct clothing for each decade from the late 1820s to the
-              1870s.
-            </p>
-            <p>
-              This particular file deals with the 1829s to 1849s, when
-              illustrations, paintings, and daguerreotypes were almost the only
-              methods of visual recording from 1830 to 1860 when better means of
-              photography became available. Each decade in this file begins with
-              images of men and families, then women, then children, from basic
-              clothing to hair styles to accessories. Each category begins with
-              fashions of ordinary, laboring people, then of wealthier people.
-              When all categories have been added to the website, anyone,
-              anywhere will have access to authentic images of clothing of much
-              of the 1800s.
-            </p>
-          </div>
-        </div>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="secondary" on:click={toggle2}>CLOSE</Button>
-      </ModalFooter>
-    </Modal>
-  </div>
+ 
 </div>
 
 <style>
